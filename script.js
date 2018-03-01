f2164247b128d931e4f4ebaa0bf08b66c46fb9b5
Bhawna Saroha <30bhawna95@gmail.com>
2018-03-01 20:34:52 +0530

    added scroll effects

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-jQuery(document).ready(function(){
-  $(window).scroll(function(){
+$(document).ready(function() {
+  $(window).scroll(function() {
     var top = $(window).scrollTop();
-    if(top>=60){
+    if (top >= 60) {
       $("nav").addClass('secondary');
-    } else if($("nav").hasClass('secondary')){
+    } else if ($("nav").hasClass('secondary')) {
       $("nav").removeClass('secondary');
     }
+  });
+
+  $(document).on('click', 'a[href^="#"]', function(event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
   });
 });
